9722987c1315b796245a327a1c216fd274c9f0c8
MicahL <micah.j.lee.1989@gmail.com>
2018-04-19 15:45:36 -0400

    Making a change in a branch

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		//Simply testing both git and Eclipse IDE.
 		System.out.println("Hello World");
 
+		//This is an addition that first originated in the "first-branch" branch.
+		System.out.println("Experimenting with branching using this program.");
 	}
 
 }
